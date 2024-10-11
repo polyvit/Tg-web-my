7d3ee9d718a8e74a2d8251eb280d4e53f9efd361
Polina Limonova <polina98_08@mail.ru>
2024-10-11 14:49:16 +0300

    Initialize project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "custom-red": "#fa5252",
+        "custom-pink": "#ff85ab"
       },
     },
   },
